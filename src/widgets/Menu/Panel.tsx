bcf431e89338c1daaec47ececc3b178999fbdf16
Levi Dev <86292578+LeviDevTele@users.noreply.github.com>
2022-01-28 20:24:31 +0530

    Update Panel.tsx

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -41,12 +41,6 @@ const Panel: React.FC<Props> = (props) => {
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
-      <a href="https://rugdoc.io/project/cronofi/">
-        <Image src="/images/rugdoc-liquid.png" width={370} height={121} responsive alt="RugDocLiquidityLocked" />
-      </a>
-      <a href="https://rugdoc.io/project/cronofi/">
-        <Image src="/images/RugDoc.png" width={370} height={121} responsive alt="RugDocReview" />
-      </a>
       <PanelFooter {...props} />
     </StyledPanel>
   );
