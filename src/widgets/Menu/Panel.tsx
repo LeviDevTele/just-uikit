bd8b02e0ebfb7628a2cb176900c583c0df390f34
Levi Dev <86292578+LeviDevTele@users.noreply.github.com>
2022-01-28 20:58:02 +0530

    Update Panel.tsx

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -40,9 +40,6 @@ const Panel: React.FC<Props> = (props) => {
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
-      <a href="https://rugdoc.io/project/onyxcash-finance/">
-        <Image src="/images/RugDoc.png" width={370} height={121} responsive alt="RugDocReview" />
-      </a>
       <PanelFooter {...props} />
     </StyledPanel>
   );
