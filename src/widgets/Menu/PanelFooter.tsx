758e63994a14dda317eba072f72eada839b028cf
Levi Dev <86292578+LeviDevTele@users.noreply.github.com>
2022-05-02 18:35:06 +0530

    Update PanelFooter.tsx

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -79,7 +79,7 @@ const PanelFooter: React.FC<Props> = ({
     <Container>
       <SocialEntry>
         {cakePriceUsd ? (
-          <PriceLink href="https://dexscreener.com/bsc/0xa59a33091577a21adb26cfb4c8275abd3095d1d2" target="_blank">
+          <PriceLink href="https://www.onyxcash.finance" target="_blank">
             <PancakeRoundIcon width="24px" mr="8px" />
             <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
           </PriceLink>
